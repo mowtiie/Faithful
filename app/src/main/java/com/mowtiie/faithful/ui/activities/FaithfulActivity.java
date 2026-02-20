e0970f6ee0507954e51440f8fb14005422367bcc
mowtiie <serbispace@proton.me>
2026-02-20 22:42:10 +0800

    Refactored abstract faithful activity class

--- app/src/main/java/com/mowtiie/faithful/ui/activities/FaithfulActivity.java
+++ app/src/main/java/com/mowtiie/faithful/ui/activities/FaithfulActivity.java
@@ -1,10 +1,9 @@
 package com.mowtiie.faithful.ui.activities;
 
-import android.content.res.Configuration;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -13,16 +12,19 @@ import com.google.android.material.color.DynamicColors;
 import com.mowtiie.faithful.R;
 import com.mowtiie.faithful.data.Theme;
 import com.mowtiie.faithful.data.Contrast;
+import com.mowtiie.faithful.util.LockUtil;
 import com.mowtiie.faithful.util.SettingUtil;
 
 public abstract class FaithfulActivity extends AppCompatActivity {
 
     protected SettingUtil settingUtil;
 
+    private boolean lockRedirectPending = false;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        settingUtil = new SettingUtil(this);
         super.onCreate(savedInstanceState);
+        settingUtil = new SettingUtil(this);
 
         if (settingUtil.isScreenPrivacyEnabled()) {
             getWindow().setFlags(
@@ -33,21 +35,40 @@ public abstract class FaithfulActivity extends AppCompatActivity {
 
         String theme = settingUtil.getTheme();
         if (theme.equals(Theme.SYSTEM.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        if (theme.equals(Theme.BATTERY_SAVING.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
-        if (theme.equals(Theme.LIGHT.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        if (theme.equals(Theme.DARK.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        else if (theme.equals(Theme.BATTERY_SAVING.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+        else if (theme.equals(Theme.LIGHT.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        else if (theme.equals(Theme.DARK.value)) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         String contrast = settingUtil.getContrast();
         if (contrast.equals(Contrast.LOW.value)) setTheme(R.style.Theme_Faithful);
-        if (contrast.equals(Contrast.MEDIUM.value)) setTheme(R.style.Theme_Faithful_MediumContrast);
-        if (contrast.equals(Contrast.HIGH.value)) setTheme(R.style.Theme_Faithful_HighContrast);
+        else if (contrast.equals(Contrast.MEDIUM.value)) setTheme(R.style.Theme_Faithful_MediumContrast);
+        else if (contrast.equals(Contrast.HIGH.value)) setTheme(R.style.Theme_Faithful_HighContrast);
 
         if (settingUtil.isDynamicColors()) DynamicColors.applyToActivityIfAvailable(this);
     }
 
     @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        recreate();
+    protected void onResume() {
+        super.onResume();
+        if (this instanceof LockActivity) return;
+
+        if (!lockRedirectPending) {
+            String passwordHash = settingUtil.getPassword();
+            if (passwordHash != null && LockUtil.getInstance().shouldLock()) {
+                lockRedirectPending = true;
+
+                Intent intent = new Intent(this, LockActivity.class);
+                intent.putExtra(LockActivity.EXTRA_RETURN_CLASS, getClass().getName());
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        lockRedirectPending = false;
+        LockUtil.getInstance().updateLastUsed();
     }
 }
--- app/src/main/java/com/mowtiie/faithful/ui/activities/SecureActivity.java
+++ app/src/main/java/com/mowtiie/faithful/ui/activities/SecureActivity.java
@@ -3,13 +3,8 @@ package com.mowtiie.faithful.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.mowtiie.faithful.R;
 import com.mowtiie.faithful.util.SettingUtil;
 
 public class SecureActivity extends AppCompatActivity {
